7451c14b15eda5ae39e1fbb2fc9510ae4208843f
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:39:37 -0300

    Update Program.cs

--- Exercicio5/Program.cs
+++ Exercicio5/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Exercício 05 Conversão de metros para Centimetros
+
+/*Console.WriteLine(" Por favor informar a metragem a ser convertida para centimetros");
+var metragem = int.Parse(Console.ReadLine());
+int metragemConvertida = metragem* 100;
