1e6ff40893f6fe708d64351af294ce02cdcc620c
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:35:31 -0300

    Update Program.cs

--- Exercicio12/Program.cs
+++ Exercicio12/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercício 12 - Área do retângulo
+Console.WriteLine("Informe o tamanho da base em centímetros");
+var base1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Informe a altura em centímetros");
+var altura = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine($"A área do retângulo é  {base1 * altura} centímetros quadrados");
+Console.WriteLine($"O perímetro do retângulo de acordo com as informações informadas possui: {2*(base1 +  altura)} centímetros quadrados");
+
+
