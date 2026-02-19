7a9e49ff01a4ce74207804b9e24a15cb64ffee57
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:36:06 -0300

    Update Program.cs

--- Exercicio13/Program.cs
+++ Exercicio13/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercício 13 -  Média de duas Provas
+
+Console.WriteLine("Informe a nota da primeira prova ");
+var nota1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe a nota da segunda prova ");
+var nota2 = int.Parse(Console.ReadLine());
+var media = (nota1 + nota2) / 2; 
+
+Console.WriteLine("A primeira nota do aluno  foi :" + nota1.ToString("F2") + Environment.NewLine + "A Segunda nota do aluno foi :" + nota2.ToString("F2") + Environment.NewLine +
+"A média entre as notas apresentadas do aluno foi " + media.ToString("F2"));
+
