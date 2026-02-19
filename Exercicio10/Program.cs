69fd51f0413c85705fb24d96312e4f33a2484928
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:34:06 -0300

    Update Program.cs

--- Exercicio10/Program.cs
+++ Exercicio10/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Exercício 10 Salário Mensal
+
+Console.WriteLine(" Qual o valor pago pela hora trabalhada?");
+var valorHora = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine(" Quantas horas foram trabalhadas no mês?");
+var horasTrabalhadas = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine($"Durante o mês foram trabalhadas {horasTrabalhadas} horas, então o valor a ser pago ao trabalhador é R$ {horasTrabalhadas*valorHora}");
+
