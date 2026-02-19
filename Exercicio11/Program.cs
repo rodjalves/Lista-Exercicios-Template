123940280a4746c0400e0b2ea556dfd2b37306dc
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:34:41 -0300

    Update Program.cs

--- Exercicio11/Program.cs
+++ Exercicio11/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercício 11 - Troco
+
+Console.WriteLine("Informe o valor do produto: ");
+var preçoProduto = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Informe o valor pago: ");
+var valorPago = Convert.ToDecimal(Console.ReadLine());
+//Console.WriteLine($"O valor do troco devido ao cliente é R$: {valorPago - preçoProduto} ");
+
+if (valorPago >= preçoProduto)
+{
+    Console.WriteLine($"O valor do troco devido ao cliente é R$: {valorPago - preçoProduto} ");
+}
+else
+{
+    Console.WriteLine(" O valor pago é inferior ao preço do produto");
+}
