d7364058333fc2aec11a0fce2b45127b405739e2
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:31:11 -0300

    Update Program.cs

--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Exercício 06 - Conversão de temperatura
+Console.WriteLine(" Informe a temperatura em graus Celsius");
+var temperatura = int.Parse(Console.ReadLine());
+var farenheit = (temperatura * 9 / 5) + 32;
+
+Console.WriteLine(" A temperatura convertida para Fahrenheit é " + farenheit);
