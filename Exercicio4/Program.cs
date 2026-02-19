4324c4bfe5e0151dd7d7627300af15c28ac4f16e
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:30:03 -0300

    Update Program.cs

--- Exercicio4/Program.cs
+++ Exercicio4/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Exercício 04 -  Calculadora Simples
+using System;
+
+Console.WriteLine(" Informe um número ");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine(" Informe outro número");
+int numero1 = int.Parse(Console.ReadLine());
+var soma = numero + numero1;
+var subtração = numero - numero1;
+var multiplicação = numero * numero1;
+var divisão =  numero / numero1;
+//int opção;
+
+Console.WriteLine(" Informe a operação que deseja fazer: 1 para somar, 2 para subtrair, 3 para  multiplicar ou  4 para dividir ");
+int opção = int.Parse(Console.ReadLine());
+
+if (opção == 1)
+{
+   Console.WriteLine(" A soma dos valores informados é: " + soma);
+}
+if (opção == 2)
+{
+    Console.WriteLine(" A subtração dos valores informados é: " + subtração);
+}
+if (opção == 3)
+{
+    Console.WriteLine(" A multipliação dos valores informados é: " + multiplicação);
+}
+if (opção == 4)
+{
+    Console.WriteLine(" A divisão dos valores informados é: " + divisão);
+}
+
+else
+    Console.WriteLine(" Opção informada inexistente");
+
