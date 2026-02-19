a88eea85b1810116ce7c762bbe95bced3cf13ba5
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:31:52 -0300

    Update Program.cs

--- Exercicio7/Program.cs
+++ Exercicio7/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercício 07 - Leitura segura de número
+Console.WriteLine(" Informe um número inteiro ");
+string dadosEntrada = (Console.ReadLine());
+try
+{
+    int numero = int.Parse(dadosEntrada);
+    Console.WriteLine($" O número inserido está de acordo {numero} ");
+}
+catch (FormatException)
+{
+
+Console.WriteLine("Erro: O número informado não parece um número inteiro válido ");
+}
+finally
+{
+    Console.WriteLine(" Final da operação da entrada de dados");
