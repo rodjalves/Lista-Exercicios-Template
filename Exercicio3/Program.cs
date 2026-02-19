a2aa0132cf42e8bcdccf185a68e5788046880a2f
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:27:26 -0300

    Update Program.cs

--- Exercicio3/Program.cs
+++ Exercicio3/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Exercício 03 - Soma de dois números
+Console.Write("Insira um número inteiro " );
+int numero = int.Parse(Console.ReadLine());
+Console.Write("Insira um novo número inteiro " );
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine($" A somatória dos números inseridos é =  { numero1 + numero} ");
