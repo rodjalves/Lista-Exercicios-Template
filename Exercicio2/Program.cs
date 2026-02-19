70fd093c52fefdd11ae73ee72482ff29413446f9
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:26:08 -0300

    Update Program.cs

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Insira um número inteiro no campo abaixo");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("O numero inserido foi" + numero1);
+Console.WriteLine($"O número antecessor ao número inserido é:  { numero1 - 1}"); 
+Console.WriteLine($"O  número sucessor ao informado pelo usuário é:  { numero1 + 1}");
