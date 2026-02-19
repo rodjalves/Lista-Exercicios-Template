59c2500a5ecf638e42901f6901889dc8ed5b0419
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:37:29 -0300

    Update Program.cs

--- Exercicio15/Program.cs
+++ Exercicio15/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercício 15 - Simulador de compra
+using System.Runtime.CompilerServices;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" Informe o nome do produto");
+        var nomeProduto = Convert.ToString(Console.ReadLine());
+        Console.WriteLine(" Informe o preço unítário do produto");
+        var preçoProduto = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(" Informe a quantidade desejada do produto");
+        var quantProduto = int.Parse(Console.ReadLine());
+        var valorTotal = quantProduto * preçoProduto;
+        Console.WriteLine(" Resumo da Compra! " + Environment.NewLine + "Nome do ítem : " + nomeProduto + Environment.NewLine+ "Quantidade: " + quantProduto + Environment.NewLine  +  "Valor total da compra " + valorTotal.ToString("C2"));
+
+    }
+}
+
+
+
