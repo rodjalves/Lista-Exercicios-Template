ff6fe7fb5cfd13e1efdf414089be6f2ee6ecd4d4
rodjalves <34447730+rodjalves@users.noreply.github.com>
2026-02-19 17:36:43 -0300

    Update Program.cs

--- Exercicio14/Program.cs
+++ Exercicio14/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Exercício 14 - Minicalculadora com método
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" Informe um número");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(" Informe um número");
+        int b = int.Parse(Console.ReadLine());
+        int resultado = somar(a, b);
+        Console.WriteLine($" O números inseridos e somados são : {resultado}");
+    }
+    static int somar(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
